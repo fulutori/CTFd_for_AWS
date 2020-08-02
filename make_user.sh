8315d60550e6d1b4deda89fd60526ca0e42a179b
aokakes <fulutori@gmail.com>
2020-08-02 19:32:34 +0900

    調整

--- make_user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo adduser ctf
-sudo gpasswd -a ctf sudo
-su - ctf
-mkdir /home/ctf/.ssh
-mv /home/ubuntu/.ssh/authorized_keys /home/ctf/.ssh/
-chown -R ctf:ctf /home/ctf/.ssh
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-/usr/bin/python3 /home/ctf/CTFd_for_AWS/get_ipv4.py
 screen -dmS ctfd
 screen -S ctfd -X stuff '/usr/bin/python3 /home/ctf/CTFd/serve.py'`echo -ne '\015'`
