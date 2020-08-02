ab7afcc7d845a888b40f913b3c98a5804c5f7425
aokakes <fulutori@gmail.com>
2020-08-02 17:22:58 +0900

    足りないものを追加

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ sudo apt -y upgrade
 
 # CTFdのインストール
 git clone https://github.com/CTFd/CTFd.git
-sudo apt insatll python3-pip
+sudo apt install python3-pip
 pip3 install -r /home/ctf/CTFd/requirements.txt
 sed -i -e "31 s/127\.0\.0\.1/0\.0\.0\.0/g" /home/ctf/CTFd/serve.py
 sed -i -e "31 s/args\.port/8000/g" /home/ctf/CTFd/serve.py
@@ -27,7 +27,7 @@ python3 /home/ctf/CTFd_for_AWS/get_ipv4.py
 sudo apt install -y nginx
 sudo sed -i -e "3 s/localhost/$server_name/g" /home/ctf/CTFd_for_AWS/nginx/default.conf
 sudo sed -i -e "4 s/localhost/$server_name/g" /home/ctf/CTFd_for_AWS/nginx/default.conf
-sudo \cp -f /home/ctf/CTFd_for_AWS/nginx/default.conf /etc/nginx/conf.d/default.conf
+sudo cp -f /home/ctf/CTFd_for_AWS/nginx/default.conf /etc/nginx/conf.d/default.conf
 sudo systemctl stop nginx
 
 # ssl証明書の設定
@@ -42,4 +42,5 @@ sudo cp -f /home/ctf/CTFd_for_AWS/nginx/ssl.conf /etc/nginx/conf.d/ssl.conf
 sudo sed -i -e "4 s/#//g" /etc/nginx/conf.d/default.conf
 sudo systemctl start nginx
 
+screen -dmS ctfd
 screen -S ctfd -X stuff '/usr/bin/python3 /home/ctf/CTFd/serve.py'
